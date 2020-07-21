1c9cbae74aa52003e5a210df899e6ebd1335a94f
yasucamp80 <yshr0403@gmail.com>
2020-07-21 22:41:18 +0900

    chatspace_incremental_search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  def index
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
 
   def edit
   end
